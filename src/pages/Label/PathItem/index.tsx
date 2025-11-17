695c39a9a99c2c0d6568a35712508e471612e753
miles <719713496@qq.com>
2025-11-17 11:25:11 +0800

    feat: 添加PathItem组件的选中状态管理功能
    - 添加selectedKeys状态跟踪选中的路径项
    - 实现点击切换选中状态功能
    - 添加选中状态的高亮样式
    - 同步Paper.js的path选中状态

--- src/pages/Label/PathItem/index.tsx
+++ src/pages/Label/PathItem/index.tsx
@@ -5,7 +5,8 @@
  * @LastEditors: didadida262
  * @LastEditTime: 2024-11-13 10:18:55
  */
-import React, { useEffect } from "react";
+import paper from "paper";
+import React, { useEffect, useState } from "react";
 
 import { ButtonCommon, EButtonType } from "@/components/ButtonCommon";
 
@@ -23,18 +24,53 @@ interface PathItemComponentProps {
 
 const PathItemComponent: React.FC<PathItemComponentProps> = (props) => {
   const { data } = props;
+  // 跟踪哪些标签被选中
+  const [selectedKeys, setSelectedKeys] = useState<Set<string>>(new Set());
+
   const handleClickPathItem = (item: PathItem) => {
+    // 切换path的selected状态
     item.path.selected = !item.path.selected;
+    
+    // 更新选中状态
+    setSelectedKeys(prev => {
+      const newSet = new Set(prev);
+      if (newSet.has(item.key)) {
+        newSet.delete(item.key);
+      } else {
+        newSet.add(item.key);
+      }
+      return newSet;
+    });
+
+    // 触发Paper.js视图更新，使选中效果可见
+    if (paper.project && paper.project.view) {
+      paper.project.view.update();
+    }
   };
-  useEffect(() => {}, [data]);
+
+  // 当data变化时，同步selectedKeys（移除不存在的item）
+  useEffect(() => {
+    const currentKeys = new Set(data.map(item => item.key));
+    setSelectedKeys(prev => {
+      const newSet = new Set<string>();
+      prev.forEach(key => {
+        if (currentKeys.has(key)) {
+          newSet.add(key);
+        }
+      });
+      return newSet;
+    });
+  }, [data]);
+
   return (
     <div className="PathItemComponent pd5">
       {data.map((item: PathItem, index: number) => {
+        const isSelected = selectedKeys.has(item.key);
         return (
-          <div className="w-full mb-[5px] rounded-[0px]" key={index}>
+          <div className="w-full mb-[5px] rounded-[0px]" key={item.key}>
             <ButtonCommon
               type={EButtonType.SIMPLE}
-              className="w-full"
+              className={isSelected ? "w-full path-item-selected" : "w-full"}
               onClick={() => handleClickPathItem(item)}
             >
               {/* {"标注数据：" + item.name.slice(0, 10) + "..."} */}
